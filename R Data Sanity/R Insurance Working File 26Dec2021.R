b0a2afe6a8322fb549952d2f39d810c56c6f12e4
Kunaal <kunaal@users.noreply.github.com>
2021-12-26 17:23:28 +0530

    Update R Insurance Working File 26Dec2021.R

--- R Data Sanity/R Insurance Working File 26Dec2021.R
+++ R Data Sanity/R Insurance Working File 26Dec2021.R
@@ -12,7 +12,8 @@ library(summarytools)
 
 ## Data Sanity
 # 1/ Missing Value Treatment - Numerical - Replace by Median, Categorical - replace by Mode
-# 2/ Transformation of Numerical/Categorical - Log Transformation, StandardScaler, one hot encoding
+# 2/ Transformation of Numerical/Categorical 
+     # - Log Transformation, StandardScaler, one hot encoding
 
 ## Advanced
 # 1/ Outlier Detection and Removal - Box Plot - Treat with Quartile 
@@ -34,50 +35,54 @@ data <- read.csv("insurance_claims.csv")
 drop <- c("X_c39")
 data = data[,!(names(data) %in% drop)]
 
-
 ##### Check Summary (summarytools)
-#summary(data)
+summary(data)
 
-#descr(data,
-#      headings = FALSE, # remove headings
-#      stats = "common" # most common descriptive statistics
-#)
+descr(data,
+      headings = FALSE, # remove headings
+      stats = "common" # most common descriptive statistics
+)
 
-#dfSummary(data)
+dfSummary(data)
 
 
 
 ##### Add Features
 
 # 1/ Add Fraud Flag
-
+data <- data %>% mutate(fraud_flag = if_else(fraud_reported == "Y", 1, 0))
 
 # 2/ Add Row Number
-
+data$row_count <- 1
 
 
 
 ##### First Code - Plot Number of Frauds Chart 
 
 # 1/ Convert Categorical feature As Factor
+data$fraud_reported <- as.factor(data$fraud_reported)
 
-
-# 2/ Set the Chart Canvas with Required Features - For Categorical just use one Feature in aes()
-
+# 2/ Set the Chart Canvas with Required Features 
+  #- For Categorical just use one Feature in aes()
+g <- ggplot(data, aes(fraud_reported))
 
 # 3/ Then add a Bar Plot
-
+g <- g + geom_bar()
+g
 
 ##### Categorical Features
 
 ### policy_state
 # 1/ Convert Categorical feature As Factor
+data$policy_state <- as.factor(data$policy_state)
 
-
-# 2/ Set the Chart Canvas with Required Features - For Categorical just use one Feature in aes()
-
+# 2/ Set the Chart Canvas with Required Features 
+#- For Categorical just use one Feature in aes()
+g <- ggplot(data, aes(policy_state))
 
 # 3/ Then add a Bar Plot
+g <- g + geom_bar()
+g
 
 
 # Other Type of Plots
@@ -91,11 +96,18 @@ mosaicplot(table(data$policy_state, data$fraud_reported),
 
 ### policy_deductable
 # 1/ Convert Categorical feature As Factor
-
+data$policy_deductable <- as.factor(data$policy_deductable)
 
 # 2/ Set the Chart Canvas with Required Features - For Categorical just use one Feature in aes()
+g <- ggplot(data, aes(policy_state)) + geom_bar()
+g
 
-
+# Other Type of Plots
+mosaicplot(table(data$policy_deductable, data$fraud_reported),
+           color = TRUE,
+           xlab = "policy_deductable", # label for x-axis
+           ylab = "Fraud" # label for y-axis
+)
 
 
 ### auto_make
@@ -118,19 +130,21 @@ mosaicplot(table(data$policy_state, data$fraud_reported),
 
 ### age
 # 1/ Set the Chart Canvas with Required Features - For Categorical just use one Feature in aes()
-
+g <- ggplot(data, aes(age))
 
 # 2/ Then add a Bar Plot
-
+g <- g + geom_histogram(binwidth = 6) 
+g
 
 ### policy_annual_premium
 # 1/ Set the Chart Canvas with Required Features - For Categorical just use one Feature in aes()
-
-
+g <- ggplot(data, aes(policy_annual_premium)) + geom_histogram(binwidth = 100)
+g
 
 ### property_claim
 # 1/ Set the Chart Canvas with Required Features - For Categorical just use one Feature in aes()
-
+g <- ggplot(data, aes(property_claim)) + geom_histogram(binwidth = 1000)
+g
 
 
 ### injury_claim
@@ -138,7 +152,20 @@ mosaicplot(table(data$policy_state, data$fraud_reported),
 
 
 
-
 ##### Check Missing Values
+sum(is.na(data))
+
+
+### Logistic Regresion
+# Training model
+logistic_model <- glm(fraud_reported ~ policy_state + age, 
+                      data = data, 
+                      family = "binomial")
+logistic_model
+
+# Summary
+summary(logistic_model)
+
+
 
 
