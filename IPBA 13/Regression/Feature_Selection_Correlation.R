65bbe9dd122db0855ea4d9f77e00924055662570
Kunaal <fxexcel@gmail.com>
2022-09-18 10:04:54 +0530

    Update Feature_Selection_Correlation.R

--- IPBA 13/Regression/Feature_Selection_Correlation.R
+++ IPBA 13/Regression/Feature_Selection_Correlation.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(ggplot2)
 library(DataExplorer)
 library(summarytools)
+library(lattice)
+library(caret)
 
 # Source : https://www.kaggle.com/code/goyalshalini93/car-price-prediction-linear-regression-rfe/data
 
@@ -12,26 +14,36 @@ setwd("D:\\GitHub\\R_Basics\\IPBA 13\\Regression")
 #Data Import
 data <- read.csv("predict_CarPrice_simple.csv", na.strings=c("NA","NaN", " "))
 
-# Empty String
-data <- data %>% mutate(across(where(is.character), ~ na_if(.,"")))
-
-##### Identify Missing Values
-colSums(is.na(data)) / nrow(data)
-
-
-# Exclude ID Column
+# Exclude ID Column from Dataset
 head(data)
 head(data[,2:ncol(data)])
 
 data_corr <- data[,2:ncol(data)]
 
+# Check Data
+plot_intro(data_corr)
 
+
+### WIP
+# Plot Correlation between Independent Variables 
+plot_correlation(data_corr)
+
+##### Select Top Features ()
+correlationMatrix <- cor(data_corr)
+
+# find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.5)
+
+# print indexes of highly correlated attributes
+print(highlyCorrelated)
+
+
+##### HeatMaps
 # rounding to 2 decimal places
 corr_mat <- round(cor(data_corr),2) 
 head(corr_mat)
 
-
-# The mtcars dataset:
+# convert to matrix
 data_corr_matrix <- as.matrix(data_corr)
 
 # Default Heatmap
@@ -39,10 +51,3 @@ heatmap(data_corr_matrix)
 
 # With Proper Scale
 heatmap(data_corr_matrix,scale="column")
-
-
-
-plot_correlation(data_corr)
-
-plot_intro(data_corr)
-plot_bar(data_corr)
