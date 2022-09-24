25641880788c068b8307ce67d2fea308e419711a
Kunaal <fxexcel@gmail.com>
2022-09-24 11:53:38 +0530

    Titanic update

---
+++ IPBA 13/Titanic/Titanic_CaseStudy_24Sep2022.R
@@ -0,0 +1,208 @@
+library(dplyr)
+library(ggplot2)
+library(DataExplorer)
+library(summarytools)
+library(reshape2)  
+
+
+##### Import & Clean
+setwd("D:\\GitHub\\R_Basics\\IPBA 13\\Titanic")
+
+#Data Import
+data <- read.csv("Titanic_train.csv")
+
+# Empty String
+data <- data %>% mutate(across(where(is.character), ~ na_if(.,"")))
+
+##### Convert String to Numerical
+# Convert Transported to 0 and 1
+data <- data %>% mutate(Transported_Flag = if_else(Transported == "True",1,0))
+
+
+##### Check Data Summary
+plot_intro(data)
+
+### What Percent columns have missing values?
+# 1.8%
+
+### What Percent columns are Discrete?
+# 53.3%
+
+### What Percent columns are Continuous?
+# 46.7%
+
+##### Exploration 
+### Identify Missing Value Columns
+plot_missing(data)
+
+
+
+## Treat Missing Values - Numerical
+
+# Get all Numerical Column Names
+plot_correlation(data, type = "continuous")
+
+# Treat Age
+data$Age <- ifelse(is.na(data$Age)
+                , ave(data$Age
+                      , FUN = function(x) mean(x, na.rm = TRUE ))
+                , data$Age)
+
+# Is Age Normal?
+hist(data$Age, breaks = 10)
+
+g <- ggplot(data, aes(Age)) + geom_histogram(bins = 10)
+g
+
+# Does is have Outliers?
+g <- ggplot(data, aes(Age)) + geom_boxplot() + coord_flip()
+g
+
+# Treat Room_Service
+# Write Code
+
+# Is Room_Service Normal?
+
+# Treat FoodCourt
+# Write Code
+
+# Is FoodCourt Normal?
+
+# Treat ShoppingMall
+# Write Code
+
+# Is ShoppingMall Normal?
+
+# Treat Spa
+# Write Code
+
+# Is Spa Normal?
+
+# Treat VRDeck
+# Write Code
+
+# Is VRDeck Normal?
+
+## Treat Missing Values - Categorical 
+
+# Get all Categorical Column Names
+colnames(data)
+names(data)
+
+# Treat HomePlanet
+data$HomePlanet <- ifelse(is.na(data$HomePlanet)
+                          , "missing", data$HomePlanet)
+
+# Times transported HomePlanet
+g <- ggplot(data, aes(HomePlanet)) + geom_bar()
+g
+
+# % transported 
+# Step 1 Summarize Data
+HomePlanet_Summary <- data %>%
+  group_by(HomePlanet)  %>%
+  summarise(
+    n_passengers = n(),
+    sum_Transported_Flag = sum(Transported_Flag)
+  )
+
+# Step 2 Create/Mutate to create % Column
+HomePlanet_Summary <- HomePlanet_Summary %>% 
+  mutate(Percent_Transported = sum_Transported_Flag/n_passengers)
+
+
+# Step 3 Plot
+g <- ggplot(HomePlanet_Summary, aes(x = HomePlanet,y = Percent_Transported)) + geom_col()
+g
+
+#Another Method 
+#ggplot(data, aes)
+
+
+# Which is the highest category?
+# Does this help predict Transported?
+
+# Treat CryoSleep
+# Write Code
+
+# Which is the highest category?
+# Does this help predict Transported?
+
+# Treat Cabin
+# Write Code
+
+# Which is the highest category?
+# Does this help predict Transported?
+
+# Treat Destination
+data$Destination <- ifelse(is.na(data$Destination)
+                          , "missing", data$Destination)
+
+# No of Destination
+g <- ggplot(data, aes(Destination)) + geom_bar()
+g
+
+# Step 1
+
+# Step 2
+
+# Step 3
+
+
+
+# Which is the highest category?
+# Does this help predict Transported?
+
+# Treat VIP
+# Write Code
+
+# Which is the higest category?
+# Does this help predict Transported?
+
+# Treat Name
+# Write Code
+
+# Which is the higest category?
+# Does this help predict Transported?
+
+
+## Transformation - Numerical
+# WHY
+data_box_plot <- select(data, c("Age","RoomService","FoodCourt","ShoppingMall","Spa","VRDeck"))
+data_long <- melt(data_box_plot)  
+
+# Applying ggplot 
+ggplot(data_long, aes(x = variable, y = value)) + geom_boxplot()
+
+# Transformation Age
+# Write here
+
+# Transformation RoomService
+# Write here
+
+# Transformation FoodCourt
+# Write here
+
+# Transformation ShoppingMall
+# Write here
+
+# Transformation Spa
+# Write here
+
+# Transformation VRDeck
+# Write here
+
+## Outlier - Numerical
+# Treat FoodCourt
+boxplot(data$FoodCourt)
+
+# Write court here
+
+#Commit Change
+data$FoodCourt <- x
+
+# Check again
+boxplot(data$FoodCourt)
+
+##### Final Check for Missing Values
+plot_missing(data)
